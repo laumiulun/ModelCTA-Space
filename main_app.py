fa825db6fbb67987652d93aba58b7a125c56138b
Andy Lau <laumiulunn@gmail.com>
2022-09-15 03:41:28 -0600

    updated file

---
+++ main_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+st.markdown(
+    """
+<style>
+body {
+    background-image: linear-gradient(#2e7bcf,#2e7bcf);
+    color: white;
+}
+</style>
+""",
+    unsafe_allow_html=True,
+)
